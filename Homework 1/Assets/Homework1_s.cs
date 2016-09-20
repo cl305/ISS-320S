361b83027e91b9c953991c9509d72e58d6f5049a
Cody Li <codyli332@gmail.com>
2016-09-19 22:59:50 -0400

    First commit

---
+++ Homework 1/Assets/Homework1_s.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Homework1_s : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		float playerHealth = 100; 
+		bool playerDamaged = true; 
+
+		if(playerDamaged){
+			playerHealth = playerHealth / 2; 
+			print("playerHealth now: " + playerHealth); 
+		}
+
+		int powerUpAmount = 50;
+		print ("powerUpAmount now: " + powerUpAmount); 
+
+		bool isDangerZone = playerHealth < 20; 
+		print ("isDangerZone now: " + isDangerZone);
+
+		playerHealth++; 
+		playerHealth += 1;
+		playerHealth = playerHealth + 1; 
+
+		print ("playerHealth now: " + playerHealth); 
+
+		int playerHealthInt = (int)playerHealth; 
+		print ("playerHealth now: " + playerHealthInt); 
+
+		print ("playerHealth mod: " + playerHealth % 2); 
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
