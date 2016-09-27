e2988d1180bb2157ea019efe5eb3c14e4c88c7ca
Cody Li <codyli332@gmail.com>
2016-09-27 12:40:20 -0400

    Adding tons of stuff

---
+++ Homework 4/Assets/CoconutDrop.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoconutDrop : MonoBehaviour {
+
+	public GameObject coconut1;
+	public GameObject coconut2; 
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
