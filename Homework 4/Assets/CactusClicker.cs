2f0fb9ae4ac1826275e50db4a182f1cb1d64cc28
Cody Li <codyli332@gmail.com>
2016-10-04 01:18:24 -0400

    Adding for loop

--- Homework 4/Assets/CactusClicker.cs
+++ Homework 4/Assets/CactusClicker.cs
@@ -9,8 +9,27 @@ public class CactusClicker : MonoBehaviour {
 	public GameObject scoreObject;
 	public ScoreDisplayer updater; 
 	// Use this for initialization
+
+
 	void Start () {
-		
+		for (int i = 0; i < 12; i++) {
+			int randNum = Random.Range (0, 3);
+			GameObject cactusCopy;
+			if (randNum == 0) {
+				cactusCopy = Instantiate (cactus1);
+			} 
+			else if (randNum == 1) {
+				cactusCopy = Instantiate (cactus2); 
+			} 
+			else {
+				cactusCopy = Instantiate (cactus3); 
+			}
+
+			float x = Random.Range (-15.0f, 15.0f);
+			float z = Random.Range (-15.0f, 15.0f);
+
+			cactusCopy.transform.position = new Vector3 (x, 0.0f, z); 
+		}
 	}
 	
 	// Update is called once per frame
