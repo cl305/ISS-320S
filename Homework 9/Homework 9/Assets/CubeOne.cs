898309734f1823c0adc5d982027935ec52337e31
Cody Li <codyli332@gmail.com>
2016-11-08 01:51:06 -0500

    Adding HW9

---
+++ Homework 9/Homework 9/Assets/CubeOne.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class CubeOne : MonoBehaviour {
+
+	Texture myTexture; 
+
+	IEnumerator GetTextureWeb(){
+		UnityWebRequest webTexture = UnityWebRequest.GetTexture("http://people.duke.edu/~djzielin/pics/dave_head2.jpg");
+		yield return webTexture.Send();
+
+		if(webTexture.isError){
+			Debug.Log(webTexture.error);
+		}
+		else{
+			myTexture = ((DownloadHandlerTexture)webTexture.downloadHandler).texture;
+			GetComponent<Renderer> ().material.mainTexture = myTexture; 
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+			StartCoroutine(GetTextureWeb());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Homework 9/Homework 9/Assets/CubeTwo.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class CubeTwo : MonoBehaviour {
+
+	Texture myTexture; 
+
+	IEnumerator GetTextureWeb(){
+		UnityWebRequest webTexture = UnityWebRequest.GetTexture("http://www.telegraph.co.uk/content/dam/news/2016/05/29/99356270_Harambe_a_17-year-old_gorilla-NEWS-large_trans++qVzuuqpFlyLIwiB6NTmJwfSVWeZ_vEN7c6bHu2jJnT8.jpg");
+		yield return webTexture.Send();
+
+		if(webTexture.isError){
+			Debug.Log(webTexture.error);
+		}
+		else{
+			myTexture = ((DownloadHandlerTexture)webTexture.downloadHandler).texture;
+			GetComponent<Renderer> ().material.mainTexture = myTexture; 
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(GetTextureWeb());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
